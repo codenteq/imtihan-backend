b3de1c90ec36a0b435257d5663e6569200bf787a
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2023-10-17 11:36:59 +0300

    EmailVerificationTest bug fix.

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -32,7 +32,11 @@ class EmailVerificationTest extends TestCase
 
         Event::assertDispatched(Verified::class);
         $this->assertTrue($user->fresh()->hasVerifiedEmail());
-        $response->assertRedirect(config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1');
+        if ($user->role === User::Admin) {
+            $response->assertRedirect(config('app.admin_frontend_url').RouteServiceProvider::HOME.'?verified=1');
+        } else {
+            $response->assertRedirect(config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1');
+        }
     }
 
     public function test_email_is_not_verified_with_invalid_hash()
