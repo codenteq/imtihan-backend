9de43696f38ea6e1fa85f2134e007f5a648be3fb
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2023-10-28 17:41:37 +0300

    role added.

---
+++ app/Http/Middleware/Role/Student.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware\Role;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Student
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->user()->role === Role::Student) {
+            abort(403);
+        }
+
+        return $next($request);    }
+}
