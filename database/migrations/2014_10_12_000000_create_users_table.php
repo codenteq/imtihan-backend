18cae603bc9c1d9011386607c7ba6395e23fe5ca
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-03-17 22:15:13 +0300

    EducationLevel enum added.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enums\EducationLevel;
+use App\Enums\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,7 +13,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        $educationLevel = [
+            EducationLevel::Primary->value,
+            EducationLevel::Middle->value,
+            EducationLevel::High->value,
+            EducationLevel::University->value,
+        ];
+
+        $role = [
+            Role::Student->value,
+            Role::Admin->value,
+        ];
+
+        Schema::create('users', function (Blueprint $table) use ($educationLevel, $role) {
             $table->id();
             $table->string('full_name');
             $table->string('email')->unique();
@@ -25,10 +39,10 @@ return new class extends Migration
             $table->foreignId('language_id')->index()->default(true);
             $table->string('avatar')->nullable();
             $table->integer('gender')->nullable();
-            $table->tinyInteger('education_level')->nullable();
+            $table->enum('education_level', $educationLevel)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->integer('role')->default(\App\Enums\Role::Student->value);
+            $table->enum('role', $role)->default(Role::Student->value);
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
