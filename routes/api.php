7c14eda486ce12cc5a0ddf3edf639101b49ac656
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-03-20 00:19:34 +0300

    api divided into student and admin.

--- routes/api.php
+++ routes/api.php
@@ -36,52 +36,11 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
         return request()->user();
     });
 
-    Route::prefix('admin')->middleware(['auth.admin', 'auth.is.active'])->group(function () {
-        Route::apiResource('users', UserController::class);
-        Route::prefix('accounts')->group(function () {
-            Route::get('/', [AccountController::class, 'show']);
-            Route::put('/', [AccountController::class, 'update']);
-            Route::put('/update-password', [AccountController::class, 'passwordUpdate']);
-            Route::delete('/', [AccountController::class, 'destroy']);
-        });
-        Route::apiResource('languages', LanguageController::class);
-        Route::apiResource('lessons', LessonController::class);
-        Route::apiResource('static-pages', StaticPageController::class);
-        Route::apiResource('announcements', AnnouncementController::class);
-        Route::apiResource('supports', SupportController::class)->only(['index', 'show', 'update', 'destroy']);
-        Route::prefix('payment')->group(function () {
-            Route::apiResource('coupons', PaymentCouponController::class);
-            Route::apiResource('methods', PaymentMethodController::class);
-            Route::apiResource('settings', PaymentSettingController::class);
-        });
-        Route::prefix('condition')->group(function () {
-            Route::apiResource('conditions', ConditionController::class);
-        });
-        Route::apiResources([
-            'questions' => QuestionController::class,
-            'question/categories' => QuestionCatergoryController::class,
-        ]);
-    });
+    Route::prefix('admin')
+        ->middleware(['auth.admin', 'auth.is.active'])
+        ->group(base_path('routes/user_types/admin.php'));
 
-    Route::prefix('student')->middleware(['auth.student', 'auth.is.active'])->group(function () {
-        Route::prefix('accounts')->group(function () {
-            Route::get('/', [\App\Http\Controllers\API\Student\Account\AccountController::class, 'show']);
-            Route::put('/', [\App\Http\Controllers\API\Student\Account\AccountController::class, 'update']);
-            Route::put('/update-password', [\App\Http\Controllers\API\Student\Account\AccountController::class, 'passwordUpdate']);
-            Route::delete('/', [\App\Http\Controllers\API\Student\Account\AccountController::class, 'destroy']);
-        });
-
-        Route::post('exams/{exam}/answer', [ExamController::class, 'storeAnswer']);
-        Route::apiResource('exams', ExamController::class)->only(['index', 'store', 'destroy']);
-        Route::get('exam-types', [ExamTypeController::class, 'index']);
-
-        Route::apiResource('supports', \App\Http\Controllers\API\Student\Support\SupportController::class)->only(['index', 'store', 'destroy']);
-        Route::apiResource('notes', NoteController::class);
-        Route::apiResource('static-pages', \App\Http\Controllers\API\Student\StaticPage\StaticPageController::class)->only(['index', 'show']);
-        Route::apiResource('class-schedules', ClassScheduleController::class);
-        Route::apiResource('announcements', \App\Http\Controllers\API\Student\Announcement\AnnouncementController::class)->only(['index', 'show']);
-        Route::get('countries', [LocationController::class, 'getCountry'])->name('country.list');
-        Route::get('cities/{countryId?}', [LocationController::class, 'getCity'])->name('city.list');
-        Route::get('states/{cityId?}', [LocationController::class, 'getState'])->name('state.list');
-    });
+    Route::prefix('student')
+        ->middleware(['auth.student', 'auth.is.active'])
+        ->group(base_path('routes/user_types/student.php'));
 });
