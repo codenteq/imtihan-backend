140076bfd4334e5a1cde7fb92e754e55f234f9e8
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-04-03 03:13:09 +0300

    update

--- tests/Browser/Admin/Announcement/AnnouncementTest.php
+++ tests/Browser/Admin/Announcement/AnnouncementTest.php
@@ -60,7 +60,7 @@ class AnnouncementTest extends AdminFrontendDuskTestCase
             $browser->click('table > tbody > tr:first-child > td > div > a')
                 ->waitFor('input[name="name"]')
                 ->type('input[name="name"]', 'Updated ' . $announcement->name)
-                ->pause(1000)
+                ->pause(2000)
                 ->press('Kaydet')
                 ->screenshot('announcements/admin/edit')
                 ->back()
