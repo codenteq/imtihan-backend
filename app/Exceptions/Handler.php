577ad07ad59b4d1707e0f71d08c8f2f46784d3f6
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-05-31 18:30:41 +0300

    sentry bug fix.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,7 +7,9 @@ use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
+use Sentry\Laravel\Integration;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -51,9 +53,10 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
+
         $this->reportable(function (Throwable $e) {
-            if (app()->bound('sentry') && app()->environment('production')) {
-                app('sentry')->captureException($e);
+            if (app()->environment('production')) {
+                Integration::captureUnhandledException($e);
             }
         });
 
