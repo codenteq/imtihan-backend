ffc26401c86c881521d637718f02bea156963100
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-08-04 23:34:50 +0300

    pause time 3ms added.

--- tests/Browser/App/Admin/Lesson/LessonTest.php
+++ tests/Browser/App/Admin/Lesson/LessonTest.php
@@ -58,7 +58,7 @@ class LessonTest extends AdminFrontendDuskTestCase
                 ->press('Kaydet')
                 ->screenshot('lessons/edit')
                 ->back()
-                ->pause(2000)
+                ->pause(3000)
                 ->waitForText('Updated '.$lesson->name, 10)
                 ->assertSeeIn('table', 'Updated '.$lesson->name)
                 ->storeConsoleLog('lessons.edit')
